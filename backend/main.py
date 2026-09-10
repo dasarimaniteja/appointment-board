d6732ef3eb8370973db74749060081b5edebb71a
dasarimaniteja <dasarimaniteja261203@gmail.com>
2026-09-10 14:17:54 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -13,10 +13,11 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Appointment Board API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+   allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://appointment-board-aabys9y3u-mt-a4c8.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
